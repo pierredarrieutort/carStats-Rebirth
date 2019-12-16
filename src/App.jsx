2d715dcdd24a7f7e3b445461ad95657af7b94c02
Pierre <p.darrieutort@outlook.fr>
2019-12-16 15:11:25 +0100

    :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import SwipeableViews from 'react-swipeable-views'
+
+class App extends Component {
+  constructor( props ) {
+
+    
+    super( props )
+
+    this.state = {
+      currentPage: 1
+    }
+
+  }
+
+
+  handleChange = el => this.setState( { currentPage: el.target.value } )
+
+  currentPage = i => this.setState( { currentPage: i } )
+
+
+  render() {
+    return (
+
+      <>
+        <nav>
+          <ul>
+            <li onClick={this.handleChange} value='0'>slider1</li>
+            <li onClick={this.handleChange} value='1'>slider2</li>
+            <li onClick={this.handleChange} value='2'>slider3</li>
+          </ul>
+        </nav>
+        <SwipeableViews enableMouseEvents index={this.state.currentPage} onChangeIndex={this.currentPage}>
+          <div className="slide slide1">slide n°1</div>
+          <div className="slide slide2">slide n°2</div>
+          <div className="slide slide3">slide n°3</div>
+        </SwipeableViews>
+      </>
+
+    )
+  }
+}
+
+export default App
